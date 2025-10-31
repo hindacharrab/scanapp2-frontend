3c1d2c8085a2e2c7012441d47e7c012dfab52fa1
hindacharrab <133971477+hindacharrab@users.noreply.github.com>
2025-10-31 10:05:12 +0100

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -3,3 +3,5 @@ export const api=axios.create({
 baseURL:"http://localhost:5088/api",// url pour mon backend 
 Headers:{"Content-Type":"application/json"},
 })
+const BASE_URL = "https://scanapp2-backend-production-df21.up.railway.app/api/scans";
+
